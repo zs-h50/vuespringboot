6992bb7ce286dd2ab9e857331e75c57128bc1947
zhushanhuan <1170047780@qq.com>
2021-07-29 18:12:41 +0800

    登录逻辑完成了，前端的登录也完善了

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.mapper.UserMapper;
 import com.example.demo.pojo.User;
 import com.example.demo.service.UserService;
+import com.example.demo.utils.Result;
 
 @Controller
 public class UserController {
@@ -17,16 +23,27 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("/logins")
-	public User Login(User user) {
-		User user2 = userService.getUserLogin(user);
+	@ResponseBody
+	@PostMapping("/login")
+	public Result Login(@RequestBody User user) {
+		System.out.println("传入的数据");
+		System.out.println(user.getAccount());
+		System.out.println(user.getPassword());
+		System.out.println(user.getIdentity());
+		System.out.println("=========================");
+		User user2 =  userService.getUserLogin(user);
+		
 		if (user2 != null) {
+			System.out.println("查询成功！");
 			System.out.println(user2.getAccount());
 			System.out.println(user2.getPassword());
 			System.out.println(user2.getIdentity());
 		}else {
-			System.out.println("查询失败!");
+			System.out.println("查询不到该用户！");
 		}
-		return user2;
+//		System.out.println(user2.getAccount());
+		System.out.println("===============================");
+		
+		return Result.success(user2);
 	}
 }
--- src/main/java/com/example/demo/service/Imp/UserServiceImp.java
+++ src/main/java/com/example/demo/service/Imp/UserServiceImp.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.Imp;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
